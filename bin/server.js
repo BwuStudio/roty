15e3fb16b23f1b03c698632eb07ad8c40b20484c
BwuNi <bwuni@outlook.com>
2019-10-14 11:25:22 +0800

    add bin/server

---
+++ bin/server.js
@@ -0,0 +1,22 @@
+// 引入koa
+const koa = require('koa');
+const path = require('path')
+const static = require('koa-static');
+
+process.title = 'roty-server';
+
+const app = new koa();
+
+
+var cors = require('koa2-cors');
+app.use(cors());
+
+// 配置静态web服务的中间件
+app.use(static(path.resolve(process.cwd(), './')));
+// 监听端口
+app.listen(3001, function () {
+    console.log('启动成功');
+})
+
+
+
